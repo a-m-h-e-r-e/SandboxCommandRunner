2b9965f166c1294ed4200fa8359583beb4f24dcb
SJ-Kheiru <heiruamir@gmail.com>
2021-03-31 10:35:15 +0300

    Second modification

--- bindings/python/CommandRunner/__init__.py
+++ bindings/python/CommandRunner/__init__.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE
+import json
 
 VERSION="0.0.1"
 
@@ -44,118 +45,117 @@ def run(
    gid=None,
    seccomp_rule=None,
 ):
-   command = [CommandRunner]
+	command = [CommandRunner]
 
-   if not isinstance(args, list):
-      raise Exception("args must be a list")
-   else:
-      for item in args:
-         if not isinstance(item, str):
-            raise Exception(item + " must be a string")
-         else:
-            command.append("--args="+item)
+	if not isinstance(args, list):
+	  raise Exception("args must be a list")
+	else:
+	  for item in args:
+	     if not isinstance(item, str):
+	        raise Exception(item + " must be a string")
+	     else:
+	        command.append("--args="+item)
 
-   if not isinstance(env, list):
-      raise Exception("args must be a list")
-   else:
-      for item in env:
-         if not isinstance(item, str):
-            raise Exception(item + " must be a string")
-         else:
-            command.append("--env="+item)
+	if not isinstance(env, list):
+	  raise Exception("args must be a list")
+	else:
+	  for item in env:
+	     if not isinstance(item, str):
+	        raise Exception(item + " must be a string")
+	     else:
+	        command.append("--env="+item)
 
-   # setting integers
-   if max_real_time is not None:
-      if not isinstance(max_real_time, int) and not None:
-         raise Exception("max_real_time must be a int")
-      else:
-         command.append("--max_real_time={}".format( max_real_time))
+	# setting integers
+	if max_real_time is not None:
+	  if not isinstance(max_real_time, int) and not None:
+	     raise Exception("max_real_time must be a int")
+	  else:
+	     command.append("--max_real_time={}".format( max_real_time))
 
-   if max_cpu_time is not None:
-      if not isinstance(max_cpu_time, int) and not None:
-         raise Exception("max_cpu_time must be an int")
-      else:
-         command.append("--max_cpu_time={}".format( max_cpu_time))
-   
-   if max_memory is not None:
-      if not isinstance(max_memory, int) and not None:
-         raise Exception("max_memory must be an int")
-      else:
-         command.append("--max_memory={}".format( max_memory))
+	if max_cpu_time is not None:
+	  if not isinstance(max_cpu_time, int) and not None:
+	     raise Exception("max_cpu_time must be an int")
+	  else:
+	     command.append("--max_cpu_time={}".format( max_cpu_time))
 
-   if max_stack is not None:
-      if not isinstance(max_stack, int) and not None:
-         raise Exception("max_stack must be an int")
-      else:
-         command.append("--max_stack={}".format( max_stack))
+	if max_memory is not None:
+	  if not isinstance(max_memory, int) and not None:
+	     raise Exception("max_memory must be an int")
+	  else:
+	     command.append("--max_memory={}".format( max_memory))
 
-   if max_output_size is not None:
-      if not isinstance(max_output_size, int) and not None:
-         raise Exception("max_output_size must be a int")
-      else:
-         command.append("--max_output_size={}".format( max_output_size))
+	if max_stack is not None:
+	  if not isinstance(max_stack, int) and not None:
+	     raise Exception("max_stack must be an int")
+	  else:
+	     command.append("--max_stack={}".format( max_stack))
 
-   if max_process_number is not None:
-      if not isinstance(max_process_number, int) and not None:
-         raise Exception("max_process_number must be a int")
-      else:
-         command.append("--max_process_number={}".format( max_process_number))
+	if max_output_size is not None:
+	  if not isinstance(max_output_size, int) and not None:
+	     raise Exception("max_output_size must be a int")
+	  else:
+	     command.append("--max_output_size={}".format( max_output_size))
 
-   if uid is not None:
-      if not isinstance(uid, int) and not None:
-         raise Exception("uid must be a int")
-      else:
-         command.append("--uid={}".format( uid))
+	if max_process_number is not None:
+	  if not isinstance(max_process_number, int) and not None:
+	     raise Exception("max_process_number must be a int")
+	  else:
+	     command.append("--max_process_number={}".format( max_process_number))
 
-   if gid is not None:
-      if not isinstance(gid, int) and not None:
-         raise Exception("gid must be a int")
-      else:
-         command.append("--gid={}".format( gid))
+	if uid is not None:
+	  if not isinstance(uid, int) and not None:
+	     raise Exception("uid must be a int")
+	  else:
+	     command.append("--uid={}".format( uid))
 
-   # setting strings
-   if exe_path is not None:
-      if not isinstance(exe_path, str):
-         raise Exception("exe_path must be a string")
-      else:
-         command.append("--exe_path=" + exe_path)
-   else:
-      raise Exception("exe_path can not be empty")
+	if gid is not None:
+	  if not isinstance(gid, int) and not None:
+	     raise Exception("gid must be a int")
+	  else:
+	     command.append("--gid={}".format( gid))
 
-   if input_path is not None:
-      if not isinstance(input_path, str):
-         raise Exception("input_path must be a string")
-      else:
-         command.append("--input_path=" + input_path)
+	# setting strings
+	if exe_path is not None:
+	  if not isinstance(exe_path, str):
+	     raise Exception("exe_path must be a string")
+	  else:
+	     command.append("--exe_path=" + exe_path)
+	else:
+	  raise Exception("exe_path can not be empty")
 
-   if output_path is not None:
-      if not isinstance(output_path, str):
-         raise Exception("output_path must be a string")
-      else:
-         command.append("--output_path=" + output_path)
+	if input_path is not None:
+	  if not isinstance(input_path, str):
+	     raise Exception("input_path must be a string")
+	  else:
+	     command.append("--input_path=" + input_path)
 
-   if log_path is not None:
-      if not isinstance(log_path, str):
-         raise Exception("log_path must be a string")
-      else:
-         command.append("--log_path=" + log_path)
+	if output_path is not None:
+	  if not isinstance(output_path, str):
+	     raise Exception("output_path must be a string")
+	  else:
+	     command.append("--output_path=" + output_path)
 
-   if error_path is not None:
-      if not isinstance(error_path, str):
-         raise Exception("error_path must be a string")
-      else:
-         command.append("--error_path=" + error_path)
+	if log_path is not None:
+	  if not isinstance(log_path, str):
+	     raise Exception("log_path must be a string")
+	  else:
+	     command.append("--log_path=" + log_path)
 
+	if error_path is not None:
+	  if not isinstance(error_path, str):
+	     raise Exception("error_path must be a string")
+	  else:
+	     command.append("--error_path=" + error_path)
 	if seccomp_rule is not None:
-		if not isinstance(seccomp_rule, str):
-			raise Exception("seccomp_rule must be a string")
-		else:
-			command.append("--seccomp_rule=" + seccomp_rule)
+	  if not isinstance(seccomp_rule, str):
+	     raise Exception("seccomp_rule must be a string")
+	  else:
+	     command.append("--seccomp_rule=" + seccomp_rule)
 
-   process = Popen(command, stdout=PIPE, stderr=PIPE)
-   stdout, stderr = process.communicate()
-   if stderr:
-      raise Exception("Error encountered while executing commandrunner: {}".format(stderr.decode("utf-8")))
-   else:
-      result = stdout.decode("utf-8")
-      return result
+	process = Popen(command, stdout=PIPE, stderr=PIPE)
+	stdout, stderr = process.communicate()
+	if stderr:
+	  raise Exception("Error encountered while executing commandrunner: {}".format(stderr.decode("utf-8")))
+	else:
+	  result = json.loads(stdout.decode("utf-8"))
+	  return result
--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -6,11 +6,10 @@ AUTHOR_EMAIL = 'heiruamir@gmail.com'
 DESCRIPTION = "Sandbox Command Runner for online judge"
 LONG_DESCRIPTION = "Python package excuting and running program file"
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+
 
 setuptools.setup(
-    name="SandboxCommandRunner", # Replace with your own username
+    name="CommandRunner",
     version=VERSION,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
@@ -19,10 +18,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
 )
